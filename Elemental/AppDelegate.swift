787fb146750f6bd6ecd63264096b8d4bd7f196c4
MilesWilliams <miles@klarity.co.za>
2017-04-30 13:52:39 +0200

    Initial Commit

---
+++ Elemental/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Elemental
+//
+//  Created by MIles Work on 2017/04/30.
+//  Copyright © 2017 StudioRepublika. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Elemental/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Elemental
+//
+//  Created by MIles Work on 2017/04/30.
+//  Copyright © 2017 StudioRepublika. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
